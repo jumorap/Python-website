05e61bb346e4b511283cac0beb5d84e0725ab496
JuanMora2017 <juan120101@hotmail.com>
2020-02-23 22:32:19 -0500

    New files, Python added

--- Python-website/index.py
+++ Python-website/index.py
@@ -10,5 +10,16 @@ def home():
 def about():
     return render_template('About.html')
 
+@app.route('/enterprise')
+def enterprise():
+    return render_template('Enterprise.html')
+
+@app.route('/help')
+def help():
+    return render_template('Help.html')
+
+@app.route('/products')
+def products():
+    return render_template('Products.html')
 if __name__ == '__main__':
     app.run(debug=True)
